1b11ebc0a6519e847cd0f59c0dd90026b467dd12
lailafarihah <lailatulfarihah278@gmail.com>
2023-12-14 19:25:29 +0700

    lutriii

---
+++ README.md
@@ -0,0 +1,3 @@
+# tugas_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tugas_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
